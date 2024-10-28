3d8a1c8bdc4ea096bc17f284f6f5cf6fbc55637f
PROYECTOEBG <156557399+PROYECTOEBG@users.noreply.github.com>
2024-10-28 10:12:25 -0500

    Update start-verificar.js

--- plugins/start-verificar.js
+++ plugins/start-verificar.js
@@ -1,172 +1,67 @@
-import { generate, generateV1, generateV2, generateV3 } from "../lib/captcha.js"
-import { createHash, randomBytes } from "crypto"
-import { getDevice } from '@whiskeysockets/baileys'
-import fetch from "node-fetch"
-import _ from "lodash"
-const Reg = /\|?(.*)([^\w\s])([0-9]*)$/i
-let msg, user, pp, who, name, age, sn, otp
-
-let handler = async function (m, { conn, text, usedPrefix, command }) {
-const dispositivo = await getDevice(m.key.id)
-user = global.db.data.users[m.sender]
-who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
-if (user.banned) return await conn.reply(m.chat, `🚫 Esta baneado. No tiene permitido usar este bot.*`, m)
-if (user.registered) return await conn.sendMessage(m.chat, { text: `${dis}Ya esta registrado como *${user.name}*\n\nSi desea hacer un nuevo registro ✨ debe de usar el comando:\n*${usedPrefix}delregistro* \`Número de serie\`\n\n🙂 Si no conoce su número de serie, use el comando:\n*${usedPrefix}numserie*`, ...fake }, { quoted: m })
-let nombre = await conn.getName(m.sender) || await generarNombreRandom()
-const edadRandom = _.random(10, 60)
-const formatoIncorrecto = `⚠️ *¡Verifica el formato de uso!*\n\n📌 Usa el comando de esta manera:\n*${usedPrefix + command} nombre.edad*\n\n📝 Ejemplo:\n*${usedPrefix + command}* ${nombre}.${edadRandom}`
-if (!Reg.test(text)) { 
-const edadesMayores = await generarEdades(18, 60)
-const edadesMenores = await generarEdades(10, 17)
-const sections = [
-{
-title: `🔢 Datos Aleatorios`,
-highlight_label: "Popular",
-rows: [{
-title: "🎲 Edad Aleatoria",
-description: `Elige ${edadRandom} para tu edad.`,
-id: `${usedPrefix + command} ${nombre}.${edadRandom}`
-}]
-},
-{
-title: `❇️ Registro Dinámico`,
-highlight_label: "Recomendado",
-rows: [{
-title: "💫 Nombre y edad aleatorios",
-description: `Registrarme como ${await generarNombreRandom()} con edad de ${edadRandom} años.`,
-id: `${usedPrefix + command} ${await generarNombreRandom()}.${edadRandom}`
-}]
-},
-{
-title: `🧓 Mayor de edad`,
-rows: edadesMayores.map(age => ({
-title: `${age} Años`,
-description: `👉 Elige ${age} para tu edad.`,
-id: `${usedPrefix + command} ${nombre}.${age}`
-}))
-},
-{
-title: `👶 Menor de edad`,
-rows: edadesMenores.map(age => ({
-title: `${age} Años`,
-description: `🍭 Elige ${age} para tu edad.`,
-id: `${usedPrefix + command} ${nombre}.${age}`
-}))
-}  
-]
-if (/ios|web|desktop|unknown/gi.test(dispositivo)) {
-return await conn.reply(m.chat, formatoIncorrecto + '\n\n' + wm2, m)
-} else {
-return await conn.sendButton(m.chat, formatoIncorrecto + '\n\n> _También puedes usar el botón de abajo..._\n', wm.trim(), null, null, null, null, [['Completar registro', sections]], m)
-}
-}  
-[, name, , age] = text.match(Reg)
-if (!name) return conn.reply(m.chat, `🫠 *No hemos econtrado su nombre, intente de nuevo.*`, m)
-if (name.length >= 41) return conn.reply(m.chat, `😩 *Use un nombre más corto por favor.*`, m)
-if (!age) return conn.reply(m.chat, `🤔 *No hemos econtrado su edad, intente de nuevo.*`, m)
-age = parseInt(age)
-if (age >= 61) return conn.reply(m.chat, `🤷‍♀️ *Use una edad más joven por favor.*`, m)
-if (age <= 9) return conn.reply(m.chat, `😆 *Use una edad mayor por favor.*`, m)
-sn = createHash('md5').update(m.sender).digest('hex')
-try {
-const { image } = await createOtp("Éxito", sn.replace(/\D/g, ""))
-let confirm = "📌 Responde este mensaje con el código OTP que aparece en la imagen."
-let txt = `🕵️‍♀️ *Proceso de Verificación* 🕵️‍♀️\n\n@${m.sender.split("@")[0]}\n${confirm}\n\n> _(El código OTP es personal y de un solo uso.)_`
-otp = sn.replace(/\D/g, "").slice(0, 6)
-msg = await conn.sendMessage(m.sender, { image: image, caption: txt, mentions: [m.sender] }, { quoted: m })
-// Si el tiempo se agota, se limpian los datos de registro
-setTimeout(() => {
-if (user.registered) return
-user.name = ""
-user.age = 0
-user.registered = false
-user.OTP = "" 
-user.registered ? '' : conn.sendMessage(m.sender, { delete: msg.key })
-}, 30000)
-m.isGroup ? await conn.reply(m.chat, "📨 El formulario de verificación se ha enviado a tu chat privado. ¡Revísalo!", m) : ''
-} catch (e) {
-msg = ''
-console.error(e)
-await conn.reply(m.chat, "*⚠️ Ocurrió un error al enviar el formulario de verificación. Intenta de nuevo más tarde.*", m)
-}
-handler.before = async function (m, { conn }) {
-let isVerified = m.quoted && m.quoted.id == msg.key.id && m.text == otp
-if (isVerified) {
-pp = await conn.profilePictureUrl(m.sender, 'image').catch(_ => yartexImg.getRandom())
-user.name = name
-user.age = age
-user.registered = true
-user.OTP = otp
-await conn.sendMessage(m.sender, { delete: msg.key })
-m.react('✨') 
-await conn.sendMessage(m.chat, { image: { url: pp }, caption: `*║⫘⫘⫘⫘⫘⫘✨*
-*║ ${dis}ＲＥＧＩＳＴＲＯ*
-*║ .・゜゜・・゜゜・．*
-*║* 💠 *Nombre* ${name}
-*║* 💠 *Edad* ${age} años
-*║* 💠 *Número de serie* \`${sn.slice(0, 6)}\`
-*║* 💠 *OTP* \`${user.OTP}\` (correcto)
-*║⫘⫘⫘⫘⫘⫘✨*\n
-> ✅ _Tus datos están seguros en nuestra base de datos y ahora puedes usar todas las funciones disponibles para usuarios verificados._`, mentions: [m.sender], ...fake }, { quoted: m })
-}}}
-handler.command = /^(ver(ify|ificar)|reg(istrar)?)$/i
-export default handler
-
-async function createOtp(buffer, code) {
-code = code.slice(0, 6)
-try {
-const captcha = await generateV2(code) || await generateV3(code) || await generateV1(code) || await generate(code)
-const captchaBuffer = captcha.buffer
-const securityBuffer = (await generateV2(buffer) || await generateV3(buffer) || await generateV1(buffer) || await generate(buffer))?.buffer
-return { image: captchaBuffer, otp: captcha.code, verified: securityBuffer }
-} catch (e) {
-console.error(e)
-}}
-
-async function generarNombreRandom() {
-const numeros = '0123456789'
-const letras = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
-const combinacion = [ numeros[_.random(0, numeros.length - 1)], numeros[_.random(0, numeros.length - 1)], letras[_.random(0, letras.length - 1)], letras[_.random(0, letras.length - 1)], letras[_.random(0, letras.length - 1)]]
-return `Usuario#${_.shuffle(combinacion).join('')}`
-}
-
-function generarEdades(min, max) {
-return _.range(max, min - 1, -1)  // Rango será de max a min (de mayor a menor)
-}
-
-/*import { createHash } from 'crypto' 
-
+import { createHash } from 'crypto'
 let Reg = /\|?(.*)([.|] *?)([0-9]*)$/i
 let handler = async function (m, { conn, text, usedPrefix, command }) {
-let user = global.db.data.users[m.sender]
-let name2 = conn.getName(m.sender)
-let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
-let pp = await conn.profilePictureUrl(who, 'image').catch(_ => yartexImg.getRandom())
-
-if (user.registered === true) return conn.sendMessage(m.chat, { text: `${dis}Ya esta registrado como *${user.name}*\n\nSi desea hacer un nuevo registro ✨ debe de usar el comando:\n*${usedPrefix}delregistro* \`Número de serie\`\n\n🙂 Si no conoce su número de serie, use el comando:\n*${usedPrefix}numserie*`, ...fake }, { quoted: m })
-if (!Reg.test(text)) return conn.reply(m.chat, `${dis}\`Uso correcto del comando:\`\n*${usedPrefix + command}* nombre.edad\n\n🩷 *Ejemplo:*\n*${usedPrefix + command}* GataDios.20`, m)
-let [_, name, splitter, age] = text.match(Reg)
-if (!name) return conn.reply(m.chat, `🫠 *No hemos econtrado su nombre, intente de nuevo.*`, m)
-if (!age) return conn.reply(m.chat, `🤔 *No hemos econtrado su edad, intente de nuevo.*`, m)
-if (name.length >= 31) return conn.reply(m.chat, `😩 *Use un nombre más corto por favor.*`, m)
-age = parseInt(age)
-if (age >= 61) return conn.reply(m.chat, `🤷‍♀️ *Use una edad más joven por favor.*`, m)
-if (age <= 10) return conn.reply(m.chat, `😆 *Use una edad mayor por favor.*`, m)
-user.name = name.trim()
-user.age = age
-user.regTime = + new Date
-user.registered = true
-let sn = createHash('md5').update(m.sender).digest('hex').slice(0, 6)	
-m.react('✨') 
-await conn.sendMessage(m.chat, { image: { url: pp }, caption: `*║⫘⫘⫘⫘⫘⫘✨*
-*║ ${dis}ＲＥＧＩＳＴＲＯ*
-*║ .・゜゜・・゜゜・．*
-*║* 💠 *Nombre* ${name}
-*║* 💠 *Edad* ${age} años
-*║* 💠 *Número de serie* \`${sn}\`
-*║⫘⫘⫘⫘⫘⫘✨*`, mentions: [m.sender], ...fake }, { quoted: m })
-  
+let fkontak = { "key": { "participants":"0@s.whatsapp.net", "remoteJid": "status@broadcast", "fromMe": false, "id": "Halo" }, "message": { "contactMessage": { "vcard": `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD` }}, "participant": "0@s.whatsapp.net" }
+  let user = db.data.users[m.sender]
+  let name2 = conn.getName(m.sender)
+  if (user.registered === true) throw `*[🛑Información 🛑]*\n\n*Usted ya esta registrado 🤨*\n\n*Use el siguiente comando para eliminar su registro*\n\n*${usedPrefix}unreg Número de serie*\n*Si no recuerda su número de serie , usar el siguiente comando 👇🏼*\n${usedPrefix}myns`
+  if (!Reg.test(text)) throw `${mg}👤 *Uso del comando correcto:* *${usedPrefix + command} nombre.edad*\n\n*•🚀 Ejemplo :* *${usedPrefix + command}* ${name2}.16`
+  let [_, name, splitter, age] = text.match(Reg)
+  if (!name) throw '*[🛑 Información 🛑]*\n\n*El nombré no puede estar vacío, por favor intenta nuevamente colocando tu nombre*'
+  if (!age) throw '*[🛑 información 🛑]*\n\n*La edad no puede estar vacía*'
+  if (name.length >= 30) throw '*El nombre es demasiado largo , intenta con un nombre mas corto*' 
+  age = parseInt(age)
+  if (age > 100) throw '*Este bot , no acepta a viejos 👻*'
+  if (age < 5) throw '*No te crear habil , y pon tu edad verdadera 🤨*'
+  user.name = name.trim()
+  user.age = age
+  user.regTime = + new Date
+  user.registered = true
+global.db.data.users[m.sender].money += 400
+global.db.data.users[m.sender].limit += 4
+global.db.data.users[m.sender].exp += 150
+global.db.data.users[m.sender].joincount += 2
+  let sn = createHash('md5').update(m.sender).digest('hex')
+await conn.sendMessage(m.chat, { 
+text: `*乂  R E G I S T R O  乂*\n
+*Nombre 👤 :* ${name}\n
+*Edad 🈴 :* ${age} años\n
+*Bono 🧿 :*\n4 Diamantes 💎\n5000 XP\n
+manda *.menu* para ver el menu`, 
+contextInfo:{
+forwardingScore: 9999999,
+isForwarded: true, 
+mentionedJid:[m.sender],
+"externalAdReply": {
+"showAdAttribution": true,
+"renderLargerThumbnail": true,
+"thumbnail": gataImg.getRandom(), 
+"title": `乂  R E G I S T R O  乂`, 
+"containsAutoReply": true,
+"mediaType": 1, 
+"mediaUrl": nnn, 
+"sourceUrl": nnn, 
 }
-handler.command = /^(ver(ify|ificar)|reg(istrar)?)$/i
+}
+}, { quoted: fkontak })  
+/* await conn.reply(m.chat, `┌───⊷ *𝐑𝐄𝐆𝐈𝐒𝐓𝐑𝐎 𝐂𝐎𝐌𝐏𝐋𝐄𝐓𝐀𝐃𝐎*
+┆ *𝐍𝐨𝐦𝐛𝐫𝐞:*
+┆ ${name}
+┆┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈
+┆ *𝐄𝐝𝐚𝐝:*
+┆ ${age} años
+┆┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈
+┆ *𝐁𝐨𝐧𝐨* 
+┆ *$4 𝐃𝐢𝐚𝐦𝐚𝐧𝐭𝐞* 💎
+┆ *$400 𝐋𝐨𝐥𝐢𝐜𝐨𝐢𝐧𝐬*
+┆ *$150 𝐗𝐏*
+┆ *$2 𝐓𝐨𝐤𝐞𝐧𝐬*
+╰──────────────────`, , m)*/
+await m.reply(`${sn}`) 
+}
+handler.help = ['daftar', 'register'].map(v => v + ' <nama>.<umur>')
+handler.tags = ['xp']
+
+handler.command = /^(verify|verificar|registrar|reg(ister)?)$/i
+
 export default handler
-*/
